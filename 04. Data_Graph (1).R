3cb8ad237f7651437789048e3f76bfb4884f3bee
juhee <mdfs0316@naver.com>
2023-10-13 11:44:51 +0900

    edit

--- 04. Data_Graph (1).R
+++ 04. Data_Graph (1).R
@@ -1,15 +1,32 @@
-#히스토그램
-hist(finedust$`3_ultrafine dust`, main="서울시 서대문구 2020년 1월 초미세먼지 측정분포", col=terrain.colors(12))
-#확률밀도선
-lines(density(finedust$`3_ultrafine dust`), lwd=2)
-#박스플롯
-boxplot(finedust$`3_fine dust`, main="야식업의 2020년 1월 미세먼지 발생현황", col="yellow")
-#박스플롯 비교
-boxplot(finedust$`3_fine dust`, finedust$`7_fine dust`, main="업종별 2020년 1월 미세먼지 발생현황", col="yellow", names = c("야식업","중식"))
-#산점도
-plot(x=finedust$`3_fine dust`, y=finedust$`3_ultrafine dust`, xlab="미세먼지", ylab="초미세먼지", main="미세먼지와 초미세먼지의 변화")
-#산점도 편집
-plot(x=finedust$`3_fine dust`, y=finedust$`3_ultrafine dust`, xlab="미세먼지", ylab="초미세먼지", main="미세먼지와 초미세먼지의 변화", pch=24, col="red", bg="yellow", cex=1.5)
-#산점도 그래프
-plot(x=finedust$`3_fine dust`, y=finedust$`3_ultrafine dust`, xlab="미세먼지", ylab="초미세먼지", main="미세먼지와 초미세먼지의 변화", type = "h")
+#파이차트
+pie(table(KOTRA2023 $진출대륙명))
+pie(table(KOTRA2023 $투자형태))
 
+#파이차트 색깔넣기
+colors <- c("red","orange","yellow","green","blue")
+pie(table(KOTRA2023 $투자형태), col=colors, main="해외진출기업의 투자형태")
+#무지개
+pie(table(KOTRA2023 $투자형태), col=rainbow(12),main="해외진출기업의 투자형태")
+#적색과 황색에 치우친 색
+pie(table(KOTRA2023 $투자형태), col=heat.colors(12),main="해외진출기업의 투자형태")
+#지구 지형 색
+pie(table(KOTRA2023 $투자형태), col=terrain.colors(12),main="해외진출기업의 투자형태")
+#앞에서 조금 더 청색에 가까운 색
+pie(table(KOTRA2023 $투자형태), col=topo.colors(12),main="해외진출기업의 투자형태")
+
+#RColorBrewer 패키지
+install.packages("RColorBrewer")
+library(RColorBrewer)
+display.brewer.all()
+
+#RColorBrewer 활용
+pal1 <- brewer.pal(5,'Set3')
+pie(table(KOTRA2023 $투자형태),col = pal1,main="해외진출기업의 투자형태")
+
+#막대그래프
+bp <- barplot(table(KOTRA2023 $진출대륙명),col=pal1, xlab="진출대륙명", ylab="진출기업수",ylim=c(0,10000))
+entry <-c(333,828,9154,104,716,444,374)
+text(x=bp, y=entry, labels=entry, pos=3)
+
+#가로 막대그래프
+barplot(table(KOTRA2023$진출대륙명),col=pal1, xlab="진출대륙명",ylab = "진출기업수",xlim = c(0,10000),horiz = TRUE)
